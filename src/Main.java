7aa6a061f837299bbf4a5f72354792e9cdd4e5c0
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-12-01 12:26:13 +0100

    Handled input parameters errors

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ public class Main {
 	public static void main( String[] args) {
 		Rope rope = new Rope();
 		String [] colors = new String[] {"Red", "Yellow", "Blue", "Brown", "White", "Grey", "Pink", "Orange"};
-		final int nMonkeys;
-		if ((args.length > 0)&&(Integer.parseInt(args[0]) < 9)) {
-			nMonkeys = Integer.parseInt(args[0]);
-		}
-		else{
-			nMonkeys = 8;
+		int nMonkeys = 8;
+		if ((args.length > 0)) {
+			try {
+				if ((Integer.parseInt(args[0]) > 0)&&(Integer.parseInt(args[0]) < 9))
+					nMonkeys = Integer.parseInt(args[0]);
+			}catch (NumberFormatException e) {
+				System.err.println("Argument must be an integer.");
+				System.exit(1);
+			}
 		}
 		Thread [] hilos = new Thread[nMonkeys];
 		Random random = new Random();
